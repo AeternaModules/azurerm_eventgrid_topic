cd6ada034ec2646490e02d70bcd5c0e1e5c79f8f
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:03:08 +0100

    Update to schema version v4.81.0

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,7 @@ output "eventgrid_topics_endpoint" {
 }
 output "eventgrid_topics_identity" {
   description = "Map of identity values across all eventgrid_topics, keyed the same as var.eventgrid_topics"
-  value       = { for k, v in azurerm_eventgrid_topic.eventgrid_topics : k => v.identity if v.identity != null && length(v.identity) > 0 }
+  value       = { for k, v in azurerm_eventgrid_topic.eventgrid_topics : k => one(v.identity) if v.identity != null && length(v.identity) > 0 }
 }
 output "eventgrid_topics_inbound_ip_rule" {
   description = "Map of inbound_ip_rule values across all eventgrid_topics, keyed the same as var.eventgrid_topics"
@@ -16,11 +16,11 @@ output "eventgrid_topics_inbound_ip_rule" {
 }
 output "eventgrid_topics_input_mapping_default_values" {
   description = "Map of input_mapping_default_values values across all eventgrid_topics, keyed the same as var.eventgrid_topics"
-  value       = { for k, v in azurerm_eventgrid_topic.eventgrid_topics : k => v.input_mapping_default_values if v.input_mapping_default_values != null && length(v.input_mapping_default_values) > 0 }
+  value       = { for k, v in azurerm_eventgrid_topic.eventgrid_topics : k => one(v.input_mapping_default_values) if v.input_mapping_default_values != null && length(v.input_mapping_default_values) > 0 }
 }
 output "eventgrid_topics_input_mapping_fields" {
   description = "Map of input_mapping_fields values across all eventgrid_topics, keyed the same as var.eventgrid_topics"
-  value       = { for k, v in azurerm_eventgrid_topic.eventgrid_topics : k => v.input_mapping_fields if v.input_mapping_fields != null && length(v.input_mapping_fields) > 0 }
+  value       = { for k, v in azurerm_eventgrid_topic.eventgrid_topics : k => one(v.input_mapping_fields) if v.input_mapping_fields != null && length(v.input_mapping_fields) > 0 }
 }
 output "eventgrid_topics_input_schema" {
   description = "Map of input_schema values across all eventgrid_topics, keyed the same as var.eventgrid_topics"
